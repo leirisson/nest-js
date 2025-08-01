986d566617d6cf26023d808ddca8a4438f8bee55
leirisson <leirissonsouza99@gmail.com>
2025-08-01 11:36:56 -0400

    deletando pastas

--- 01-primeira-API-loja/loja/src/app.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common'
-import { AccountModule } from './account/account.module'
-import { ProductModule } from './product/product.module'
-
-@Module({
-  imports: [AccountModule, ProductModule],
-})
-export class AppModule {}
